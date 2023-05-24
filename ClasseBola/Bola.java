49e23366ecee2c9e78b34d4c87e415ef3de6b9d1
Manoel-DJS <vinesouza9x8@gmail.com>
2023-05-24 16:26:04 -0300

    Questão 1

---
+++ ClasseBola/Bola.java
@@ -0,0 +1,18 @@
+package ClasseBola;
+
+public class Bola {
+    private String cor;
+    private double circunferencia;
+    private String material;
+    
+    public void trocaCor(String novaCor){
+        cor = novaCor;
+    }
+
+    public String mostraCor(){
+        return cor;
+    }
+
+    
+}
+
---
+++ ClasseBola/ProgramaBola.java
@@ -0,0 +1,10 @@
+package ClasseBola;
+
+public class ProgramaBola {
+    public static void main(String[] args) {
+        Bola b1 = new Bola();
+
+        b1.trocaCor("Vermelho");
+        System.out.println(b1.mostraCor());
+    }
+}
